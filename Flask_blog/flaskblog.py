e6f246055e90b17ac17195f1839b092137925aca
SAFNAS <iamsafnas@gmail.com>
2020-06-13 12:06:38 +0530

    show blog in html

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Safnas',
+        'title':'Blog Post',
+        'content':'First Blog Post',
+        'date_posted':'June 13, 2020'
+    },
+    {
+        'author':'Safnas',
+        'title':'Blog Post 2',
+        'content':'Second Blog Post',
+        'date_posted':'June 15, 2020'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
