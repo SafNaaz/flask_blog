4f217e87d2c5019b5fb6a26f6fccc4b807401f84
SAFNAS <iamsafnas@gmail.com>
2020-06-13 16:57:40 +0530

    register, login form html & email_validator installed

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5ed25becb3b0a153dbe985e8ccf76764'
@@ -26,6 +28,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
