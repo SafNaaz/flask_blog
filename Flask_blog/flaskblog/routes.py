b083dbe6bd7b1d915b68201855bcde13c576a3f1
SAFNAS <iamsafnas@gmail.com>
2020-06-14 21:29:10 +0530

    proper restructuring

--- Flask_blog/flaskblog/routes.py
+++ Flask_blog/flaskblog/routes.py
@@ -1,15 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '5ed25becb3b0a153dbe985e8ccf76764'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-db = SQLAlchemy(app)
-
-from models import User,Post
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.models import User,Post
+from flaskblog.forms import RegistrationForm, LoginForm
 
 posts = [
     {
@@ -53,6 +45,3 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password','danger')
     return render_template('login.html', title='Login', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True)
