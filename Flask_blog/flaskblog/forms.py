38557a919527ccff31badc514e5b642e9dc8c62a
SAFNAS <iamsafnas@gmail.com>
2020-06-16 19:56:05 +0530

    file upload form and extension type validation

--- Flask_blog/flaskblog/forms.py
+++ Flask_blog/flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField,PasswordField,SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -54,6 +55,9 @@ class UpdateAccountForm(FlaskForm):
     email = StringField('Email', validators=[
         DataRequired(),
         Email()
+    ])
+    picture = FileField('Update Profile Picture', validators=[
+        FileAllowed(['jpg','png'])
     ])
     submit = SubmitField('Update')
 
