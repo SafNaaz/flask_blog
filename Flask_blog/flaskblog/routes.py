c99e1ee39c6bd60830cf85901c0280d22d6762a6
SAFNAS <iamsafnas@gmail.com>
2020-06-17 00:26:42 +0530

    changed pagination count

--- Flask_blog/flaskblog/routes.py
+++ Flask_blog/flaskblog/routes.py
@@ -11,7 +11,7 @@ import os
 @app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=2)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
