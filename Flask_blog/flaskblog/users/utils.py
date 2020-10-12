20f667408f307ccd5798b2f1ecfa4c5366c52b72
Safnas <iamsafnas@gmail.com>
2020-10-13 00:56:05 +0530

    configuration for app

--- Flask_blog/flaskblog/users/utils.py
+++ Flask_blog/flaskblog/users/utils.py
@@ -11,7 +11,7 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(
-        app.root_path, 'static/profile_pics', picture_fn)
+        current_app.root_path, 'static/profile_pics', picture_fn)
     # output_size = (125,125)
     # i = image.open(form_picture)
     # i.thumbnail(output_size)
